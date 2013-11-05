325c3be215f1f18af2c6936639d3f05cc681dc2b
action121 <wuxiaoming@wuxiaomingtekiMacBook-Pro.local>
2013-11-05 10:48:35 +0800

    Initial Commit

---
+++ crashHandler/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  crashHandler
+//
+//  Created by 吴晓明 on 13-11-5.
+//  Copyright (c) 2013年 吴晓明. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ crashHandler/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  crashHandler
+//
+//  Created by 吴晓明 on 13-11-5.
+//  Copyright (c) 2013年 吴晓明. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
